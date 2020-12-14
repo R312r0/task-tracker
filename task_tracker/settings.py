2372b9facbb45f7eeb91a53401db5a3506900b28
R312r0 <vladyslav.chupovskiy@gmail.com>
2020-12-15 00:14:23 +0200

    Setting up files for deploy again

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -46,13 +46,14 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'task_tracker.urls'
@@ -124,12 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 STATIC_URL = '/static/'
-
-
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
